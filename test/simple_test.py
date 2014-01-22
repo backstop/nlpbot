a09b6ebca51f8db8ac62e226408410c469155a1d
fcaimi <fedeoasi@gmail.com>
2014-01-22 11:28:06 -0600

    Add simple test.

--- test/simple_test.py
+++ test/simple_test.py
@@ -5,6 +5,8 @@ import nlpbot
 class TestSimple(unittest.TestCase):
     def test_simple_learn_and_gen(self):
         nlpbot.learn('This is a test sentence.')
-        from nlpbot.core import db
-        print db
         self.assertEqual(nlpbot.generate_output(), 'This is a test sentence .')
+
+    def test_learn_and_gen_two_options(self):
+        nlpbot.learn('This is a wonderful test sentence.')
+        self.assertEqual(nlpbot.generate_output(), 'This is a wonderful test sentence .')
